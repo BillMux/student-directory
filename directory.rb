e6bf9d3afaf39b1f0890febc54875671e84752d8
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-09-25 17:29:49 +0000

    save_students saves input into a csv file

--- directory.rb
+++ directory.rb
@@ -1,13 +1,18 @@
 @students = []
+
 def interactive_menu
     @students = []
     loop do
         print_menu
-    # 2. read the input and save it into a variable
         process(gets.chomp)
-        # 3. do what the user has asked
-
     end
+end
+
+def print_menu
+    puts "1. Input students"
+    puts "2. Display students"
+    puts "3. Save list to students.csv"
+    puts "9. Exit" # 9 because we'll be adding more items
 end
 
 def process(selection)
@@ -16,6 +21,8 @@ def process(selection)
             input_students
         when '2'
             show_students
+        when '3'
+            save_students
         when '9'
             exit # this will cause the program to terminate
         else
@@ -66,11 +73,6 @@ def print_footer(students)
     puts
 end
 
-def print_menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-end
 
 def show_students
     print_header
@@ -101,6 +103,19 @@ def group_by_cohort
     end
   end
   puts
+end
+
+def save_students
+    # open the file for writing
+    file = File.open("students.csv" , "w")
+    
+    # iterate over the array of students
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(',')
+        file.puts csv_line
+    end
+    file.close
 end
 
 interactive_menu
