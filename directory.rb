822a09189e677c34bfc8ffce96629bd020b2e1cc
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-09-25 18:14:47 +0000

    standard input added to gets

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def interactive_menu
     @students = []
     loop do
         print_menu
-        process(gets.chomp)
+        process(STDIN.gets.chomp)
     end
 end
 
@@ -37,14 +37,14 @@ def input_students
     puts "Please enter the names of the students, followed by their cohort"
     puts "To finish, just hit return twice"
     @students = []
-    name = gets.chomp
+    name = STDIN.gets.chomp
     while !name.empty? do
         puts "Now enter their cohort"
-        cohort = gets.chomp
+        cohort = STDIN.gets.chomp
         @students << {name: name, cohort: cohort.to_sym}
         puts "Now we have #{@students.count} students"
         puts "Enter another student?"
-        name = gets.chomp
+        name = STDIN.gets.chomp
     end
 end
 
@@ -121,13 +121,25 @@ def save_students
     file.close
 end
 
-def load_students
+def load_students(filename = "students.csv")
     file = File.open("students.csv", "r")
     file.readlines.each do |line|
         name, cohort = line.chomp.split(',')
         @students << {name: name, cohort: cohort.to_sym}
     end
     file.close
+end
+
+def try_load_students
+    filename = ARGV.first # first argument from the command line
+    return if filename.nil? # get out of the method if it isn't given
+    if File.exists?(filename) # if it exists
+        load_students(filename)
+        puts "Loaded #{students.count} from #{filename}"
+    else # if it doesn't exist
+        puts "Sorry, #{filename} doesn't exist"
+        exit
+    end
 end
 
 interactive_menu
